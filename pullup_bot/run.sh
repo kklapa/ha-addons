33a5e255e7afc3b205089c97872cfab206164dac
kklapa <95380832+kklapa@users.noreply.github.com>
2025-08-15 01:45:01 +0300

    Update run.sh

--- pullup_bot/run.sh
+++ pullup_bot/run.sh
@@ -1,6 +1,3 @@
-#!/usr/bin/env bash
-export BOT_TOKEN="${BOT_TOKEN}"
-export GUILD_ID="${GUILD_ID}"
-export CHANNEL_ID="${CHANNEL_ID}"
-python bot.py
-
+#!/usr/bin/with-contenv bashio
+bashio::log.info "Pull-Up Bot starting..."
+python3 /app/bot.py
